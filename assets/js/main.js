f7f69a80b3f1b48210fe8acb7cb094af95af2a16
Martina Monti <164319633+martimonti@users.noreply.github.com>
2024-06-27 22:30:06 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,6 +110,22 @@
   }, true)
 
   /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
    * Activate/show sections on load with hash links
    */
   window.addEventListener('load', () => {
