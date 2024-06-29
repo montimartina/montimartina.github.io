5c02f4c6dbbecee115724f9e9bb927040147a8ed
Martina Monti <martinamonti866449@gmail.com>
2024-06-29 16:48:33 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'martina.monti1@unimi.it';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
